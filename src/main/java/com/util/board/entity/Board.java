23bbf291c55d0fb2f37af0e5117dd42def2b326c
coding-Rhino <qkdrb1234@naver.com>
2024-10-01 11:05:43 +0900

    feat: #3 BoardEntity 구현

---
+++ src/main/java/com/util/board/entity/Board.java
@@ -0,0 +1,55 @@
+package com.util.board.entity;
+
+import com.util.audit.Auditable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Board extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long boardId;
+
+    @Column(name = "board_title")
+    private String title;
+
+    @Column(name = "board_content")
+    private String content;
+
+    @Column(name = "board_views")
+    private int views = 0;
+
+    @Column(name = "comment_count")
+    private int commentCount = 0;
+
+    @Column(name = "board_category")
+    private String category;
+
+    @Column(name = "board_image_url")
+    @ElementCollection
+    private List<String> imageUrl = new ArrayList<>();
+
+    @Column(name = "employee_id")
+    private Long employeeId;
+
+    @Column(name = "employee_name")
+    private String employeeName;
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<BoardComment> boardCommentList = new ArrayList<>();
+
+    public void setBoardComments(BoardComment boardComment) {
+        boardCommentList.add(boardComment);
+        if (boardComment.getBoard() != this) {
+            boardComment.setBoard(this);
+        }
+    }
+}
---
+++ src/main/java/com/util/board/entity/BoardComment.java
@@ -0,0 +1,56 @@
+package com.util.board.entity;
+
+import com.util.audit.Auditable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class BoardComment extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long boardCommentId;
+
+    @Column(name = "board_comment_content")
+    private String content;
+
+    @Column(name = "employee_id")
+    private Long employeeId;
+
+    @Column(name = "employee_name")
+    private String employeeName;
+
+    @ManyToOne
+    @JoinColumn(name = "BOARD_ID")
+    private Board board;
+
+    public void setBoard(Board board) {
+        this.board = board;
+        if (!board.getBoardCommentList().contains(this)) {
+            board.setBoardComments(this);
+        }
+    }
+
+    public BoardComment(Long boardCommentId) {
+        this.boardCommentId = boardCommentId;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_comment_id")
+    private BoardComment parentComment;
+
+    @OneToMany(mappedBy = "parentComment", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<BoardComment> replies = new ArrayList<>();
+
+    public void addReply(BoardComment reply) {
+        replies.add(reply);
+        reply.setParentComment(this);
+    }
+}
