f524d22203dbe7103978b4a930165a104f430215
coding-Rhino <qkdrb1234@naver.com>
2024-10-01 12:21:00 +0900

    feat: #3 BoardMapper 구현

---
+++ src/main/java/com/util/board/mapper/BoardMapper.java
@@ -0,0 +1,21 @@
+package com.util.board.mapper;
+
+import com.util.board.dto.BoardDto;
+import com.util.board.entity.Board;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {BoardCommentMapper.class})
+public interface BoardMapper {
+
+    Board boardPostDtoToPost(BoardDto.Post requestBody);
+
+    Board boardPatchDtoToBoard(BoardDto.Patch requestBody);
+
+    @Mapping(target = "boardCommentList", qualifiedByName = "boardCommentToBoardCommentResponse")
+    BoardDto.Response boardToBoardResponseDto(Board board);
+
+    List<BoardDto.Response> boardsToBoardResponseDtos(List<Board> boards);
+}
