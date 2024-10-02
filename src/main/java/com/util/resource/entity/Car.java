8b06696b2d769b4103a08905966450289d75d4b5
coding-Rhino <qkdrb1234@naver.com>
2024-10-02 16:15:13 +0900

    feat : #5 ResoureceDto 구현, Entity 수정

--- src/main/java/com/util/resource/entity/Car.java
+++ src/main/java/com/util/resource/entity/Car.java
@@ -5,8 +5,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @Getter
 @Setter
@@ -23,6 +23,12 @@ public class Car {
     @Column(name = "car_number")
     private String number;
 
+    @ElementCollection
+    @CollectionTable(name = "car_images", joinColumns = @JoinColumn(name = "car_id"))
+    @MapKeyColumn(name = "car_image_url")
+    @Column(name = "car_image_description")
+    private Map<String, String> images = new HashMap<>();
+
     @Enumerated(EnumType.STRING)
     @Column(name = "car_fuel_type")
     private fuelType fuel = fuelType.GASOLINE;
@@ -30,25 +36,11 @@ public class Car {
     @Column(name = "car_available")
     private boolean available = true;
 
-    @OneToMany(mappedBy = "car", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-    private List<CarImage> images = new ArrayList<>();
-
-    public void addImage(CarImage image) {
-        images.add(image);
-        image.setCar(this);
-    }
-
-    public void removeImage(CarImage image) {
-        images.remove(image);
-        image.setCar(null);
-    }
-
     public enum fuelType {
         GASOLINE("휘발유"),
         DIESEL("경유"),
         LPG("LPG"),
         ELECTRIC("전기");
-
 
         @Getter
         private final String fuel;
