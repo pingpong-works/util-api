b9ec35f867ae418d37f6ceec97ec8ff37f319fca
quokkavely <yellowjerri@gmail.com>
2024-10-10 02:09:18 +0900

    fix #17 :send alarm for total employees

--- src/main/java/com/util/feign/EmployeeFeignClient.java
+++ src/main/java/com/util/feign/EmployeeFeignClient.java
@@ -4,10 +4,14 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
 import java.util.Map;
 
-@FeignClient(name = "employee-service")
+@FeignClient(name = "auth-api")
 public interface EmployeeFeignClient {
     @GetMapping("/employees/{id}")
     Map<String, Object> getEmployeeById(@PathVariable("id") Long employeeId);
+
+    @GetMapping("/employees")
+    List<Long> getEmployeeIds();
 }
